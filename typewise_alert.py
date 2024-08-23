713672ec8a29dfc5235bbd1d55b68b8b977f84dc
shc32 <shc146388@gmail.com>
2024-08-23 14:42:01 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -12,13 +12,10 @@ def classify_temperature_breach(coolingType):
   lowerLimit = 0
   upperLimit = 0
   if coolingType == 'PASSIVE_COOLING':
-    lowerLimit = 0
     upperLimit = 35
   elif coolingType == 'HI_ACTIVE_COOLING':
-    lowerLimit = 0
     upperLimit = 45
   elif coolingType == 'MED_ACTIVE_COOLING':
-    lowerLimit = 0
     upperLimit = 40
   return lowerLimit, upperLimit
 
